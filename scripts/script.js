0ca4e48a34fd62bac72b28507bab03fc6ab7506d
Demi Broere <90050856+DemiBroere@users.noreply.github.com>
2021-12-27 19:08:58 +0100

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+
+/* stop animation and transition when resizing the page */
+
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.   body.classList.remove("resize-animation-stopper");
+  }, 400);
+});
+
+/* Button click menu */ 
+
+var menuButton = document.querySelector("nav button");
+var menuSee = document.querySelector("nav ul");
+
+function toonMenu() {
+    menuSee.classList.toggle("terug");
+}
+
+menuButton.addEventListener("click", toonMenu);
